0509a024fea37b219fe1adcf546e6cd371b845da
nguyen.quoc.vietb <nguyen.quoc.viet-b@sun-asterisk.com>
2021-01-25 13:25:54 +0700

    [33721]Log in and log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,32 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token, :reset_token
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  before_save :downcase_email
+
+  validates :name, presence: true
+  validates :email, presence: true,
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :password, presence: true
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
+
+  has_secure_password
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def downcase_email
+    email.downcase!
+  end
 end

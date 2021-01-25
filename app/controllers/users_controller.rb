3a0c12e13f7d2e7f3e2c423a9eb6f0aff5733a6a
nguyen.quoc.vietb <nguyen.quoc.viet-b@sun-asterisk.com>
2021-01-25 13:25:54 +0700

    [33457] Authentication (login, logout, forget/reset password, remember)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,75 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new create)
+  before_action :load_user, except: %i(index new create)
+
+  def index
+    @users = User.paginate(page: params[:page],
+      per_page: Settings.paginate.users)
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t("message.user_warning")
+    redirect_to root_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = t "message.check_mail"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = t "message.update_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "message.delete_success"
+    else
+      flash[:failed] = t "message.delete_failed"
+    end
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit :name, :email, :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "message.alert_login"
+    redirect_to login_url
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t "message.user_warning"
+    redirect_to(root_path) unless current_user? @user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   scope "(:locale)", locale: /en|vi/ do
     root "pages#index"
     get "store" => "pages#store"
     get "blog" => "pages#blog"
     get "suggest" => "pages#suggest"
     get "favorites" => "pages#favorites"
+    get "/login", to: "session#new"
+    post "/login", to: "session#create"
+    delete "logout", to: "session#destroy"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users
+    resources :account_activations, only: :edit
+    resources :password_resets, except: %i(index show destroy)
   end
 end

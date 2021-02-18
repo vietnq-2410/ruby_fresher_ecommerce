9773c5fef972d331daa7e3530bee24b30c7424bf
nguyen.quoc.vietb <nguyen.quoc.viet-b@sun-asterisk.com>
2021-02-18 13:53:47 +0700

    [33997] gem devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,77 +1,21 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
   has_many :orders, dependent: :destroy
   enum role: {user: 0, admin: 1}
   attr_accessor :remember_token, :activation_token, :reset_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   before_save :downcase_email
-  before_create :create_activation_digest
 
   validates :name, presence: true
   validates :email, presence: true,
-    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+            format: {with: VALID_EMAIL_REGEX}, uniqueness: true
   validates :password, presence: true
-
-  def self.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create string, cost: cost
-  end
-
-  has_secure_password
-
-  def forget
-    update_column :remember_digest, nil
-  end
-
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
-
-  def remember
-    self.remember_token = User.new_token
-    update_column :remember_digest, User.digest(remember_token)
-  end
-
-  def authenticated? attribute, token
-    digest = send "#{attribute}_digest"
-    return false if digest.nil?
-
-    BCrypt::Password.new(digest).is_password? token
-  end
-
-  def activate
-    update_columns activated: true, activated_at: Time.zone.now
-  end
-
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
-
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_columns reset_digest: User.digest(reset_token),
-      reset_sent_at: Time.zone.now
-  end
-
-  def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
-  end
-
-  def password_reset_expired?
-    reset_sent_at < Settings.reset_pass_expired_time.hours.ago
-  end
 
   private
 
   def downcase_email
     email.downcase!
-  end
-
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,17 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    devise_for :users
     root "pages#index"
     get "store" => "pages#store"
     get "blog" => "pages#blog"
     get "suggest" => "pages#suggest"
     get "favorites" => "pages#favorites"
-    get "/login", to: "session#new"
-    post "/login", to: "session#create"
-    delete "logout", to: "session#destroy"
-    get "/signup", to: "users#new"
-    post "/signup", to: "users#create"
-    resources :users
     resources :cart, only: %i(index create update)
     resources :order, only: %i(index create)
     resources :products, only: :show
+    resources :users
     post "remove-cart-item/:id" => "cart#remove_cart_item", as: "remove_cart_item"
     get "remove-cart-all-item" => "cart#remove_all_item"
-    resources :account_activations, only: :edit
     resources :password_resets, except: %i(index show destroy)
     namespace :admin do
       get "/" => "admins#dashboard"
